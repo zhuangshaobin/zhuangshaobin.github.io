3427eb8b5b6b7f20a204b99a08aabdb7066d64a0
zhuangshaobin <94739615+zhuangshaobin@users.noreply.github.com>
2026-03-16 10:57:57 +0000

    Deploying to gh-pages from @ zhuangshaobin/zhuangshaobin.github.io@ed951fd25aa056c2f1e3204060276b150c4e0760 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "Repositories",
           description: "Some of my open-source projects and selected contributions.",
